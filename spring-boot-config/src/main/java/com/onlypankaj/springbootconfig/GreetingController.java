c14f1c484921fcfe5a19bef1a1730182bcb5f028
onlypankaj <onlypankaj@gmail.com>
2021-06-13 23:56:50 +0530

    property can be used as map to read multiple values, using Spring expression language(SPEL)

--- spring-boot-config/src/main/java/com/onlypankaj/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/onlypankaj/springbootconfig/GreetingController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class GreetingController {
@@ -15,10 +16,14 @@ public class GreetingController {
     @Value("${my.list.values}")
     private List<String> listValues;
 
+    @Value("#{${dbValues}}")
+    private Map<String, String> dbValues;
+
     @GetMapping("/greeting")
     public String greeting(){
 
         return this.greetingMessage+
-                this.listValues;
+                this.listValues+
+                this.dbValues;
     }
 }
