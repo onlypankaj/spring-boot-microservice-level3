0b2e4c45d3bd245be2055c9be76194abb9645475
onlypankaj <onlypankaj@gmail.com>
2021-06-13 23:46:18 +0530

    Spring takes values from property as list directly

--- spring-boot-config/src/main/java/com/onlypankaj/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/onlypankaj/springbootconfig/GreetingController.java
@@ -4,14 +4,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class GreetingController {
 
     @Value("${app.descriptionnew:default value}")
     private String greetingMessage;
 
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
     @GetMapping("/greeting")
     public String greeting(){
-        return this.greetingMessage;
+
+        return this.greetingMessage+
+                this.listValues;
     }
 }
