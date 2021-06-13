0eaa0785524d9323d78c9c5c1c3835b3135a58a8
onlypankaj <onlypankaj@gmail.com>
2021-06-13 23:18:23 +0530

    Regular read directly passing value hard coded

--- spring-boot-config/src/main/java/com/onlypankaj/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/onlypankaj/springbootconfig/GreetingController.java
@@ -1,4 +1,17 @@
 package com.onlypankaj.springbootconfig;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingController {
+
+    @Value("New Hello")
+    private String greetingMessage;
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return this.greetingMessage;
+    }
 }
